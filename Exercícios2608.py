6728dde731a4d458d4bcf0b1464b3c40f59b8591
ViitzyzZ <braiantomasi0018@outlook.com>
2020-08-31 14:58:19 -0300

    exercicio

---
+++ Exercícios2608.py
@@ -0,0 +1,18 @@
+#Exercício 1
+numeroInteiro = 2
+print(numeroInteiro)
+
+#Exercício 2
+numeroReal = 4
+print(numeroReal)
+
+#Exercício 3
+numeroA = 0
+numeroB = 0
+numeroC = 0
+
+armazenaValorTecladoInt = int(input("Digite um número inteiro: "))
+armazenaValorTecladoInt = int(input("Digite um número inteiro: "))
+armazenaValorTecladoInt = int(input("Digite um número inteiro: "))
+print(numeroA + numeroB + numeroC)
+
