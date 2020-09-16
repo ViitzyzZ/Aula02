bd32acd7cef5c293d60599770e7ea80513480506
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-16 16:54:18 -0300

    Aula07-while

---
+++ Aula07-while.py
@@ -0,0 +1,26 @@
+#while usamos enquanto a condição for verdadeira(loop infinito)
+i=0;
+#while i <= 6:
+ #   print("loop infinito",i)
+  #  i=i+1
+    #ou
+    #1+=1
+    
+#While loop infinito(com parada satisfaz a condição )
+while i<6:
+    print("Loop elegante",i)
+    i=i+1
+    #ou
+    #i+=1
+#While com loop infinito e if com break para quebrar o laço de repetição(while)
+x=0
+while x<5:
+    print("Loop mais elegante ainda",x)
+    if x==3:
+        break
+    x=x+1
+
+
+
+
+
