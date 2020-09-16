deb58861a825ea2c5f301232f53b33704ad7ca4c
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-16 16:52:55 -0300

    Aula07-for

---
+++ Aula07-for.py
@@ -0,0 +1,35 @@
+#for é um laço ou estrutura de interação.
+
+#Percorrer uma lista.
+minhaLista = ["Java", "JS", "Dart", "Ruby", "Python"]
+for x in minhaLista:
+    print("Minha Linguagem: ", x)
+
+#Percorrer uma palavra
+for x in "Python":
+    print("Letras da palavra: ", x)
+
+#Parar de percorrer a lista(sair do loop) quando encontrar a palavra Dart.
+for x in minhaLista:
+    print("Minhas linguagens: ", x)
+    if x=="Dart":
+        break
+
+#Usano função range() para percorrer o for.
+for x in range(5):
+    print("Usando função range(): ",x)
+else:
+    print("Acabou a execução do loop for!")
+
+#Usando função range() definindo intervalo.
+for x in range(2,6):
+    print("Usando função range() definindo início e fim: ",x)
+
+#Usando função range() com incremento personalizado.
+for x in range(5,50,10):
+    print("Usando função range() defininindo início, fim e incremento personalizado: ",x)
+
+#Se criar um for sem bloco de código, tem que usar o pass, senão vai dar erro de sintaxe. 
+for x in minhaLista:
+    pass
+
